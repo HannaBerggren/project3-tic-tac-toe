4559b0688cd3beee57a274a7b23d4e537fcf344d
HannaBerggren <hannajberggren@gmail.com>
2023-04-19 11:01:50 +0000

    Create game function to welcome player and made imports for functions, variables and animation

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from time import sleep # welcome title animation
+import time # game statement to disappear after a few seconds
+import sys # to access functions and variables
+
+
+# Welcome title with animation
+
+welcome_title = "Welcome to play Tic Tac Toe game!"
+
+for x in welcome_title:
+    print(x, end='')
+    sys.stdout.flush()
+    sleep(.1)
