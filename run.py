4e6e858453864713c1e1ba4954c9b4e2907f09d1
HannaBerggren <hannajberggren@gmail.com>
2023-04-19 11:17:44 +0000

    Create main variables for the game

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import time # game statement to disappear after a few seconds
 import sys # to access functions and variables
 
 
-# Welcome title with animation
+# welcome title with animation
 
 welcome_title = "Welcome to play Tic Tac Toe game!"
 
@@ -11,3 +11,14 @@ for x in welcome_title:
     print(x, end='')
     sys.stdout.flush()
     sleep(.1)
+
+
+# main variables for the gameboard, player's move, winners name and scores
+# to access them inside the functions using the global statement
+
+player_move = 'x'
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+winner = None
+name = None
+x_score = 0
+o_score = 0
